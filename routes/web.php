4063634050cce5952dd58d9be9aa01c30a435a67
unknown <Stalin Reascos@TISISTEMAS.hpdkuio.local>
2025-05-14 13:44:26 -0500

    instalacion_breeze

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
 use App\Http\Controllers\Dashboard\ProductController;
-use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\Dashboard\VisitorController;
 use App\Http\Controllers\Dashboard\ZoneController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('zone', ZoneController::class);
-Route::resource('user', UserController::class);
-Route::resource('visitor', VisitorController::class);
-Route::resource('product', ProductController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('visitor', VisitorController::class);
+    Route::resource('zone', ZoneController::class);
+    Route::resource('product', ProductController::class);
+});
+
+require __DIR__.'/auth.php';
