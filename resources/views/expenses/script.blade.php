2463a10586c8e5275eaa1ae677b9470539c3229d
stalinc2k <stalin_c2k@hotmail.com>
2025-06-07 09:01:24 -0500

    Agrega script.blade.php de gastos

---
+++ resources/views/expenses/script.blade.php
@@ -0,0 +1,31 @@
+<script>
+function cerrarModalPDF() {
+            const modal = document.getElementById('modalPDF'); // <-- esta línea faltaba
+            const iframe = document.getElementById('iframePDF');
+            iframe.src = '';
+            modal.classList.add('hidden');
+            modal.classList.remove('flex');
+        }
+
+        function mostrarModal(id) {
+            const modal = document.getElementById(id);
+            modal.classList.remove('hidden');
+            modal.classList.add('flex');
+        }
+
+        function cerrarModal(id) {
+            const modal = document.getElementById(id);
+            modal.classList.remove('flex');
+            modal.classList.add('hidden');
+            location.reload();
+        }
+
+        function abrirModalPDF(expense) {
+            const iframe = document.getElementById('iframePDF');
+            const modal = document.getElementById('modalPDF');
+            const url = '{{ route('expense.expense', ['expense' => ':expense']) }}'.replace(':expense', expense);
+            iframe.src = url;
+            modal.classList.remove('hidden');
+            modal.classList.add('flex');
+        }
+</script>
